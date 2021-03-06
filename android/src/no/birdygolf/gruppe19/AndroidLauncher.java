eb4d59f01577cfcec5e19d29ff4bab12016a7e95
Petter Charles Stellberg Redfern <petter.charles@redfern.no>
2021-03-06 11:32:46 +0100

    Set up project

---
+++ android/src/no/birdygolf/gruppe19/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package no.birdygolf.gruppe19;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import no.birdygolf.gruppe19.BirdyGolf;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BirdyGolf(), config);
+	}
+}
---
+++ desktop/src/no/birdygolf/gruppe19/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package no.birdygolf.gruppe19.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import no.birdygolf.gruppe19.BirdyGolf;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BirdyGolf(), config);
+	}
+}
