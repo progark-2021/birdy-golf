1c4b8ec4b360675c9d1910df91ab9778c268408f
Petter Charles Stellberg Redfern <petter.charles@redfern.no>
2021-03-06 14:17:57 +0100

    Add title screen

--- core/src/no/birdygolf/gruppe19/BirdyGolf.java
+++ core/src/no/birdygolf/gruppe19/BirdyGolf.java
@@ -1,27 +1,29 @@
 package no.birdygolf.gruppe19;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class BirdyGolf extends ApplicationAdapter {
-	SpriteBatch batch;
+import no.birdygolf.gruppe19.screen.TitleScreen;
 
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-	}
+public class BirdyGolf extends Game {
+    public SpriteBatch batch;
+    public OrthographicCamera camera;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+        setScreen(new TitleScreen(this));
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
 }
