4fb8b23e586a1d615e85fde28ca3c0bc096de06e
Petter Charles Stellberg Redfern <petter.charles@redfern.no>
2021-03-09 13:48:15 +0100

    Add packages for components and systems

--- core/src/no/birdygolf/gruppe19/BirdyGolf.java
+++ core/src/no/birdygolf/gruppe19/BirdyGolf.java
@@ -1,11 +1,7 @@
 package no.birdygolf.gruppe19;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import no.birdygolf.gruppe19.screen.TitleScreen;
