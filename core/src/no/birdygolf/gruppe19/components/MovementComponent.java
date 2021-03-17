3eff005141c3acb515a9ac8d1e0c5776b6290092
haldisborgen <haldisborgen@gmail.com>
2021-03-17 15:40:54 +0100

    added movementsystem

---
+++ core/src/no/birdygolf/gruppe19/components/MovementComponent.java
@@ -0,0 +1,9 @@
+package no.birdygolf.gruppe19.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.math.Vector2;
+
+public class MovementComponent implements Component {
+    public final Vector2 velocity = new Vector2();
+    public final Vector2 accel = new Vector2();
+}
---
+++ core/src/no/birdygolf/gruppe19/systems/MovementSystem.java
@@ -0,0 +1,36 @@
+package no.birdygolf.gruppe19.systems;
+
+import com.badlogic.ashley.core.ComponentMapper;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.gdx.math.Vector2;
+
+import no.birdygolf.gruppe19.components.MovementComponent;
+import no.birdygolf.gruppe19.components.TransformComponent;
+
+public class MovementSystem extends IteratingSystem {
+    private Vector2 tmp = new Vector2();
+
+    private ComponentMapper<TransformComponent> tm;
+    private ComponentMapper<MovementComponent> mm;
+
+    public MovementSystem() {
+        super(Family.all(TransformComponent.class, MovementComponent.class).get());
+
+        tm = ComponentMapper.getFor(TransformComponent.class);
+        mm = ComponentMapper.getFor(MovementComponent.class);
+    }
+
+    @Override
+    public void processEntity(Entity entity, float deltaTime) {
+        TransformComponent pos = tm.get(entity);
+        MovementComponent mov = mm.get(entity);;
+
+        tmp.set(mov.accel).scl(deltaTime);
+        mov.velocity.add(tmp);
+
+        tmp.set(mov.velocity).scl(deltaTime);
+        pos.pos.add(tmp.x, tmp.y, 0.0f);
+    }
+}
