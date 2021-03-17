690b11db2774b5f1a9c3dac71f5a18fab1217db3
Ronja Ek <rlek@stud.ntnu.no>
2021-03-17 16:15:25 +0100

    Changes to GameScreen

--- core/src/no/birdygolf/gruppe19/screen/GameScreen.java
+++ core/src/no/birdygolf/gruppe19/screen/GameScreen.java
@@ -1,17 +1,28 @@
 package no.birdygolf.gruppe19.screen;
 
+import com.badlogic.ashley.core.PooledEngine;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 
 import no.birdygolf.gruppe19.BirdyGolf;
+import no.birdygolf.gruppe19.factory.WorldFactory;
+import no.birdygolf.gruppe19.systems.MovementSystem;
 
 public class GameScreen extends ScreenAdapter {
 
     BirdyGolf game;
+    WorldFactory world;
+    PooledEngine engine;
 
     public GameScreen(BirdyGolf game) {
         this.game = game;
+        engine = new PooledEngine();
+        world = new WorldFactory(engine);
+
+        engine.addSystem(new MovementSystem());
+
+        world.create();
     }
 
     @Override
