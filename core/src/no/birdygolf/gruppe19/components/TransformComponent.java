c7ab4eab4419c084f06fd980dde74aa4de4576e8
annesofj <annesofj@stud.ntnu.no>
2021-03-21 15:34:27 +0100

    Fixed bug in movementSystem

--- core/src/no/birdygolf/gruppe19/components/TransformComponent.java
+++ core/src/no/birdygolf/gruppe19/components/TransformComponent.java
@@ -6,8 +6,12 @@ import com.badlogic.gdx.math.Vector3;
 
 // Describing the balls rotation
 public class TransformComponent implements Component {
-    public final Vector3 pos = new Vector3();
+    public final Vector2 currentPos = new Vector2();
     public final Vector2 scale = new Vector2(1.0f, 1.0f);
     public float rotation = 0.0f;
+
+    // FROM SET PRESSED
+    public Vector2 pressedPosition = new Vector2();
+    public boolean wasPressed = false;
 }
 
